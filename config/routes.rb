f25fc450995f87d0f85095e7db70e92d0ef53b7f
bibigan <jace.jingjing.zhai@gmail.com>
2025-02-20 17:12:30 -0500

    delete greeter controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   get "countries/index", to: "countries#index"
-  get "greeter/hello", to: "greeter#hello"
-
-  get "greeter/goodbye"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
