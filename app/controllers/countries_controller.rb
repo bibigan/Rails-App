c4534fe7bbcf926ae1cb8bad8d59390982289c47
bibigan <jace.jingjing.zhai@gmail.com>
2025-02-21 19:10:13 -0500

    add default searching description

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,8 +2,9 @@ class CountriesController < ApplicationController
   def index
     # Get user's search terms,
     # if no search then default results will be shown for “ca”.
-    @search_term = params[:looking_for] || "ca"
-    @countries = Country.search(@search_term)
+    @search_term = params[:looking_for]
+    @search_term_des = @search_term.present? ? @search_term : "All"
+    @countries = @search_term.present? ? Country.search(@search_term) : Country.all
   end
 
   def details
